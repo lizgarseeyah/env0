7b6c1c7db68aa3ac5562ef2d616234cbbb1b9f44
Liz Garcia <lizgarseeyah@gmail.com>
2022-03-04 19:23:50 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  access_key = "ACCESSKEY"
+  secret_key = "SECRETKEY"
+  version = "~> 2.0"
+  region  = "us-east-1"
+  assume_role {
+      role_arn    = "arn:aws:iam::768631217593:role/env0-service-role"
+      session_name = "terraform"
+      external_id = "my_external_id"
+  }
+}
+
+
+
+resource "aws_s3_bucket" "env0" {
+  bucket = "BUCKETNAME"
+
+}
+
+provider "random" {}
+
+resource "random_string" "random" {
+  length = "16"
+  special = "false"
+  min_lower = "16"
+}
+
+resource "aws_s3_bucket" "takehometask_bucket" {
+  bucket = "env0-s3-${random_string.random.result}"
+  acl    = "public-read"
+  force_destroy = true
+}
